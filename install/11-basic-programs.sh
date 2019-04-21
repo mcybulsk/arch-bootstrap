f830811a2cab29fb6e079096b1aa5bb51623faba
Michał Cybulski <michal@cybulski.me>
2019-04-21 10:05:35 +0200

    Add basic programs

---
+++ install/11-basic-programs.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+source utils/source-common.sh
+
+echo "Install yay"
+git clone https://aur.archlinux.org/yay.git
+pushd yay
+makepkg -si
+popd
+
+echo "Install a couple of base programs"
+sudo pacman -S \
+	firefox \
+	flameshot \
+	keepass
+
+echo "Install Dropbox"
+yay -S dropbox
+rm -rf ~/.dropbox-dist
+install -dm0 ~/.dropbox-dist
+
+echo "Install signal and slack"
+yay -S signal-desktop-bin slack-desktop
+
---
+++ install/README.md
@@ -0,0 +1,8 @@
+# How to use
+
+Steps 1-3 should be run directly from the default Arch zsh console
+Steps 4-9 should be run from arch-chroot
+Before step 10 there should be a reboot
+Step 10 should be run as root
+Steps from 11 should be run as the user
+
